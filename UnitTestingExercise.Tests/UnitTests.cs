c385a1fb5b6d4bba4f777d0396c3b36c500ffcd1
SlowMint <individualmotiv@gmail.com>
2022-08-10 20:02:20 -0500

    initial commit

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,88 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1,2,3,6)]
+        [InlineData(3,6,4,13)]
+        [InlineData(-8,-2,15,5)]
+        [InlineData(2,23,-47, -22)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var addTest = new UnitTestMethods();
             //Act
-
+            var actual = addTest.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(3,2,1)]
+        [InlineData(15,8,7)]
+        [InlineData(22,-70,92)]
+        [InlineData(-7,8,-15)]
+        [InlineData(76,222,-146)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
-
+            var subtractTest = new UnitTestMethods();
             //Act
-
+            var actual = subtractTest.Subtract(minuend, subtrhend);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(3,4,12)]
+        [InlineData(5,-5,-25)]
+        [InlineData(17,76,1292)]
+        [InlineData(-1,0,0)]
+        [InlineData(-16,-16,256)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            var multiplyTest = new UnitTestMethods();
             //Act
-
+            var actual = multiplyTest.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2,2,1)]
+        [InlineData(16,2,8)]
+        [InlineData(-28,7,-4)]
+        [InlineData(-30,-5,6)]
+        [InlineData(17,0,0)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            var divideTest = new UnitTestMethods();
             //Act
-
+            var actual = divideTest.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(expected,actual);
         }
 
         [Fact]
-        public void YourMethodName()
+        public void HelloWorldTest()
         {
             //Arrange
-
+            var helloTest = new UnitTestMethods();
             //Act
-
+            var actual = "Hello World!";
             //Assert
-
+            Assert.Equal("Hello World!", actual);
         }
 
         [Fact]
-        public void YourMethodName2()
+        public void ReturnFiveTest()
         {
             //Arrange
-
+            var fiveTest = new UnitTestMethods();
             //Act
-
+            var actual = 5;
             //Assert
+            Assert.Equal(5, actual);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -10,17 +10,47 @@ namespace UnitTestingExercise
 
         // Create an Add method that passes 3 integers
 
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
         // Create a Subtract method that passes 2 integers
             // Keep track of which number is getting passed as minuend and subtrahend
 
+        public int Subtract (int a, int b)
+        {
+            return a - b;
+        }
+
         // Create a Multiply method that passes 2 integers
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
 
         // Create a Divide method that passes 2 integers
 
-
+        public int Divide(int a, int b)
+        {       
+            if (b == 0)
+            {
+                return 0;
+            }
+            else return a / b;
+        }
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        public string HelloWorld()
+        {
+            return "Hello World!";
+        }
 
+        public int ReturnFive()
+        {
+            return 10 - 5;
+        }
         
     }
 }
